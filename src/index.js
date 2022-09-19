0516039d69144e9457490eacc61802c832c911ef
Joe Scott <jbs198585@gmail.com>
2022-09-19 16:34:44 -0400

    Reconfigured components again

--- src/index.js
+++ src/index.js
@@ -3,22 +3,9 @@ import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './routes/Home';
-import Skills from './routes/Skills';
-import Projects from './routes/Projects';
-import Contact from './routes/Contact';
-
 render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route path="home" element={<Home />} />
-        <Route path="skills" element={<Skills />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="contact" element={<Contact />} />
-      </Route>
-    </Routes>
-  </Router>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.querySelector('#root')
 )
