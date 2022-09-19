7a1360cdd453621d412372534875fce56015e342
Joe Scott <jbs198585@gmail.com>
2022-09-19 16:02:59 -0400

    Set up routes in index.js, set up routes folder

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Skills from './routes/Skills';
+import Projects from './routes/Projects';
+import Contact from './routes/Contact';
+
 render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="home" element={<Home />} />
+        <Route path="skills" element={<Skills />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+    </Routes>
+  </Router>,
   document.querySelector('#root')
 )
