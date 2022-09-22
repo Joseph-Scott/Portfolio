3accdef48c2e90c332d7dbfab16e00b0bd731ddf
Joe Scott <jbs198585@gmail.com>
2022-09-22 06:19:25 -0400

    Rendering issue

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
-function App() {
+const App = () => {
   return (
     <div>
       <Navigation />
