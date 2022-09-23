fdcfaa35a728d8cb6f9de07f176e8f200ac8791c
Joe Scott <jbs198585@gmail.com>
2022-09-23 07:56:15 -0400

    Figuring out best way to use Bootstrap

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 
 const Navigation = () => {
   return (
-    <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/Home">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/Skills">Skills</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/Projects">Projects</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/Contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 };
 
